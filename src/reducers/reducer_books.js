0a2209c7731cc8483a55da302274762b27fad2d9
Jair Reina <Jair.Reina@digitaslbi.com>
2017-10-01 20:40:51 -0500

    adding books reducer, creating booklist container and rendering the list of books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default ()=>{
+	return [
+		{title: 'Cien años de soledad'},
+		{title: 'Harry Potter y el monge que vendio su ferrari'},
+		{title: 'Quien quiere ser trillonario'},
+		{title: 'Who the hell am I?' }
+	];
+}
